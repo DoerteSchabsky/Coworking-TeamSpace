ebe381ffd5a6ff309ebde5e04831bfbce95bc58d
DoerteSchabsky <doerte@workinn.de>
2026-07-04 00:06:01 +0200

    04.07. Quality Check 1

---
+++ Sprites/Mitarbeiter/Mitarbeiter & Biber.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ChatGPT Image 3. Juli 2026, 23_23_00" tilewidth="32" tileheight="32" tilecount="1536" columns="48">
+ <image source="ChatGPT Image 3. Juli 2026, 23_23_00.png" width="1536" height="1024"/>
+</tileset>
