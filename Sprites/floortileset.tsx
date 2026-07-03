4d0367ed8a2bdf60c1ee56788463d778fb104749
DoerteSchabsky <doerte@workinn.de>
2026-07-04 00:27:49 +0200

    tiles

---
+++ Sprites/floortileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="floortileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../floortileset.png" width="272" height="272"/>
+</tileset>
