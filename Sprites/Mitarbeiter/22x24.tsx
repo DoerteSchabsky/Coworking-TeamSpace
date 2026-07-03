5610d26773a95478120b9b604566f3bb52f68bbd
DoerteSchabsky <78438228+DoerteSchabsky@users.noreply.github.com>
2026-07-03 22:07:46 +0000

    Deploying to gh-pages from master @ ebe381ffd5a6ff309ebde5e04831bfbce95bc58d 🚀

---
+++ Sprites/Mitarbeiter/22x24.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ChatGPT Image 3. Juli 2026, 23_23_00" tilewidth="22" tileheight="24" tilecount="2898" columns="69">
+ <image source="ChatGPT Image 3. Juli 2026, 23_23_00.png" width="1536" height="1024"/>
+</tileset>
---
+++ Sprites/Mitarbeiter/ChatGPT Image 3. Juli 2026, 23_36_41.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ChatGPT Image 3. Juli 2026, 23_36_41" tilewidth="32" tileheight="32" tilecount="1519" columns="49">
+ <image source="ChatGPT Image 3. Juli 2026, 23_36_41.png" width="1586" height="992"/>
+</tileset>
